f0d22416da8ba2cfb31d9db0e5cbc32b52aa5099
unknown <theodkul@gmail.com>
2020-02-06 19:02:09 -0500

    Add Simulation interface

---
+++ src/driver/Simulation.java
@@ -0,0 +1,16 @@
+package Driver;
+
+import org.opencv.core.*;
+
+/**Interface representing a specific type of simulation.
+*/
+
+public interface Simulation {
+	/**"Runs" this simulation by drawing a gradient on the given base picture using the given rotation and translation matrices.
+	@param basePicture The base picture to draw on.
+	@param rotationMatrix The rotation matrix.
+	@param translationMatrix The translation matrix.
+	@return A mat consisting of the base picture with the gradient drawn on it.
+	*/
+	Mat run(Mat basePicture, Mat rotationMatrix, Mat translationMatrix);
+}
