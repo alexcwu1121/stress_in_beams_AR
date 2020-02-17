a59eac843e5304dd0023d37db612f3efba32bd66
unknown <nicholasmataczynski0617@gmail.com>
2020-02-16 20:07:02 -0500

    Initial complete version

--- src/crosssection/gridSpace.java
+++ src/crosssection/gridSpace.java
@@ -1,33 +1,50 @@
 package crosssection;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.awt.Dimension;
-import javax.swing.*;
 
-public class gridSpace extends JPanel{
+public class gridSpace{
 
     private Rectangle box;
     private Color color;
 
+    /**
+    constructor method that initilizaes the gridSpace rectangle.
+    @param x1 The x position of the top right corner of the rectangle.
+    @param y1 The y position of the top right corner of the rectangle.
+    @param x2 The x length of the rectangle.
+    @param y2 The y length of the rectangle.
+    @author Nicholas Mataczynski
+    */
     public gridSpace(int x1, int y1, int x2, int y2){
         box = new Rectangle(x1, y1, x2, y2);
     }
 
-    public void setData(int x1, int y1, int x2, int y2){
-        box = new Rectangle(x1, y1, x2, y2);
-    }
-
+    /**
+    Sets the color of the gridSpace object
+    @param r The red value of the color.
+    @param b The blue value of the color.
+    @param g The green value of the color.
+    @author Nicholas Mataczynski
+    */
     public void setColor(int r, int b, int g){
         color = new Color(r, b, g);
     }
 
+    /**
+    Returns the gridSpace rectangle
+    @return The rectangle of the gridSpace
+    @author Nicholas Mataczynski
+    */
     public Rectangle getRect(){
         return box;
     }
 
+    /**
+    Returns the color of the gridSpace
+    @return The color of the gridSpace
+    @author Nicholas Mataczynski
+    */
     public Color getColor(){
         return color;
     }
--- src/crosssection/plane.java
+++ src/crosssection/plane.java
@@ -8,21 +8,21 @@ import java.awt.Rectangle;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import java.util.HashMap;
-
 import crosssection.gridSpace;
-import userinterface.settingsWindow;
 
 public class plane extends JPanel {
-  	private static gridSpace boxes[][] = new gridSpace[10][10];
-  	private static HashMap<String, String> settings;
+  	public int stressCap = 5;
+  	public int xSpaces = 10;
+  	public int ySpaces = 10;
+  	public gridSpace boxes[][];
+  	public JFrame frame;
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        for (int i = 0; i < 10; i++){
-    		for (int j = 0; j < 10; j++){
+        for (int i = 0; i < xSpaces; i++){
+    		for (int j = 0; j < ySpaces; j++){
     			g2.setColor(boxes[i][j].getColor());
     			g2.fill(boxes[i][j].getRect());
     		}
@@ -30,56 +30,59 @@ public class plane extends JPanel {
     }
 
     /**
-    Updates the colors of all boxes in the grid then repaints the JFrame <br>
-    @param frame The JFrame that the drawing is going to take place on.
+    Creates the array of rectangles which make up the crosssection grid
+    @param frame_ The JFrame that the drawing is going to take place on.
+    @param xSpaces_ The amount of gridSpaces that will be generated every row.
+    @param ySpaces_ The amount of gridSpaces that will be generated every column.
 	@author Nicholas Mataczynski
     */
-    public static void planeUpdate(JFrame frame) {
-    	for (int i = 0; i < 10; i++){
-    		for (int j = 0; j < 10; j++){
-    			boxes[i][j].setColor(1, 1, 1);
-    		}
-        }
-        frame.repaint();
-    }
-
-    /**
-    Creates the array of rectangles which make up the crosssection grid <br>
-    @param frame The JFrame that the drawing is going to take place on.
-	@author Nicholas Mataczynski
-    */
-    public static void planeInit(JFrame frame) {
+    public plane(JFrame frame_, int xSpaces_, int ySpaces_) {
+    	xSpaces = xSpaces_;
+    	ySpaces = ySpaces_;
+    	frame = frame_;
 
     	Rectangle r = frame.getBounds();
 		int h = r.height;
 		int w = r.width;
+		int xLength = 300 / xSpaces;
+		int yLength = 300 / ySpaces;
 
-    	for (int i = 0; i < 10; i++){
-    		for (int j = 0; j < 10; j++){
-    			boxes[i][j] = new gridSpace(w - i * 30 - 50, j * 30 + 10, 30, 30);
-    			boxes[i][j].setColor(i * 20, j * 20, i + j * 20);
+		boxes = new gridSpace[xSpaces][ySpaces];
+
+    	for (int i = 0; i < xSpaces; i++){
+    		for (int j = 0; j < ySpaces; j++){
+    			boxes[i][j] = new gridSpace(w - i * xLength - 50, j * yLength + 10, xLength, yLength);
     		}
         }
     }
 
-  	/**
-	Class which creates the crosssection shape <br>
+    /**
+    Updates the colors of all gridSpaces in the grid then repaints the JFrame
+    @param frame The JFrame that the drawing is going to take place on.
+    @param vecX The X rotational vector
+    @param vecY The Y rotational vector
 	@author Nicholas Mataczynski
-  	*/
-	public static void main(String[] args) {
+    */
+    public void planeUpdate(JFrame frame,  int vecX[], int vecY[]) {
 
-		//settings = settingsWindow.generateGUI();
+    	int xStress = vecX[0] - vecY[0];
+    	int yStress = vecX[1] - vecY[1];
+    	int middle = xSpaces / 2;
+    	int xMagnifier = (125 / stressCap) / (xSpaces / 2);
+    	int yMagnifier = (125 / stressCap) / (ySpaces / 2);
 
-	    JFrame frame = new JFrame("Crosssection");
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    plane grid = new plane();
-	    frame.add(grid);
-	    frame.setSize(400, 400);
-	    frame.setLocationRelativeTo(null);
-
-	    planeInit(frame);
-
-	    frame.setVisible(true);
-
-  	}
+    	for (int i = 0; i < xSpaces; i++){
+    		for (int j = 0; j < ySpaces; j++){
+    			int xColor = (middle - i) * Math.min(Math.max(xStress, -stressCap), stressCap) * xMagnifier;
+    			int yColor = (middle - j) * Math.min(Math.max(yStress, -stressCap), stressCap) * yMagnifier;
+    			int combinedColor = xColor - yColor;
+    			if (combinedColor >= 0){
+    				boxes[i][j].setColor(255, 255 - combinedColor, 255 - combinedColor);
+    			} else {
+    				boxes[i][j].setColor(255 - Math.abs(combinedColor), 255 - Math.abs(combinedColor), 255);
+    			}
+    		}
+        }
+        frame.repaint();
+    }
 }
